e3caea5cf22f6d640c4f6c0caf7d3c02a51e3526
wiz <luisdabarbosa@gmail.com>
2024-08-24 09:39:21 -0500

    open in page

--- tabs.js
+++ tabs.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tabList = document.getElementById("tabList");
-  const openPageButton = document.getElementById("openPage");
-
-  openPageButton.addEventListener("click", () => {
-    chrome.tabs.create({ url: "tabs.html" });
-  });
 
   chrome.tabs.query({}, (tabs) => {
     tabs.forEach((tab) => {
